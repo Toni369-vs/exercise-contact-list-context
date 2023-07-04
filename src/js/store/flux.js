d6716d7814e59bbc80c4003a8e22a5ca0174298a
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-07-04 08:27:53 +0000

    Se agregan nuevas funciones

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,15 +1,47 @@
+
+
 const getState = ({ getStore, setStore }) => {
 	return {
-		store: {},
+		store: {
+			contacts: []
+		},
 
 		actions: {
 			getAllContacts: () => {
-				fetch("https://assets.breatheco.de/apis/fake/contact/agenda", {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/{agenda_slug}", {
 					method: "GET"
+				})
+					.then(response => {
+						if (!response.ok) {
+							throw new Error("Error al obtener los contactos");
+						}
+						return response.json();
+					})
+					.then(data => {
+						setStore({ contacts: data });
+						console.log(data);
+					})
+					.catch(error => console.log(error));
+			},
+			// CREAR CONTACTO
+
+			createContact: (full_name, address, phone, email) => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"full_name": full_name,
+						"address": address,
+						"agenda_slug": "my_super_agenda",
+						"phone": phone,
+						"email": email,
+					}),
 				})
 				.then(response => {
 					if (!response.ok) {
-						throw new Error("Error al obtener las agendas");
+						throw new Error("Error al crear el contacto");
 					}
 					return response.json();
 				})
@@ -19,13 +51,21 @@ const getState = ({ getStore, setStore }) => {
 				.catch(error => console.log(error));
 			},
 
-			createContact: contactData => {
+			// MODIFICAR CONTACTO
+
+			updateContact: (full_name, address, phone, email) => {
 				fetch("https://assets.breatheco.de/apis/fake/contact/", {
-					method: "POST",
+					method: "PUT",
 					headers: {
 						"Content-Type": "application/json"
 					},
-					body: JSON.stringify(contactData)
+					body: JSON.stringify({
+						"full_name": full_name,
+						"address": address,
+						"agenda_slug": "ToniCM",
+						"phone": phone,
+						"email": email,
+					}),
 				})
 				.then(response => {
 					if (!response.ok) {
